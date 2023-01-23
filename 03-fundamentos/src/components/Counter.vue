0819ef9c30a3273e4c0ede8a6916e0ff047274e7
Juan Carlos Santamaría Villegas <jsantamv@gmail.com>
2023-01-23 15:30:59 -0600

    volar

--- 03-fundamentos/src/components/Counter.vue
+++ 03-fundamentos/src/components/Counter.vue
@@ -1,11 +1,11 @@
 <template>
-  <h2>{{ customTitle }}</h2>
-  <p>{{ counter }} <sup>2</sup> = {{ squereCounter }}</p>
+    <h2>{{ customTitle }}</h2>
+    <p>{{ counter }} <sup>2</sup> = {{ squereCounter }}</p>
 
-  <div>
-    <button @click='increaseCounter'>+1</button>
-    <button @click="decreaseCounter">-1</button>
-  </div>
+    <div>
+        <button @click='increaseCounter'>+1</button>
+        <button @click="decreaseCounter">-1</button>
+    </div>
 
 </template>
 
@@ -13,55 +13,55 @@
 //import { throwStatement } from '@babel/types';
 
 export default {
-  props: {
-    title: String,
-    start: {
-      type: Number,
-      default: 100,
-      //required: true
-      validator(value){
-        return value >=0
-      }
-    }
-  },
-  data() {
-    return {
-      counter: this.start,
-    };
-  },
-  methods: {
-    getSquareValue() {
-      return this.counter;
+    props: {
+        title: String,
+        start: {
+            type: Number,
+            default: 100,
+            //required: true
+            validator(value) {
+                return value >= 0
+            }
+        }
     },
-    /** Method to Increase counter */
-    increaseCounter() { this.counter++ },
-    /** Method to Decrease counter */
-    decreaseCounter() { this.counter-- },
-  },
-  computed: {
-    squereCounter() {
-      return this.counter * this.counter;
+    data() {
+        return {
+            counter: this.start,
+        };
     },
-    customTitle() { return this.title || 'Counter' }
-  },
+    methods: {
+        getSquareValue() {
+            return this.counter;
+        },
+        /** Method to Increase counter */
+        increaseCounter() { this.counter++ },
+        /** Method to Decrease counter */
+        decreaseCounter() { this.counter-- },
+    },
+    computed: {
+        squereCounter() {
+            return this.counter * this.counter;
+        },
+        customTitle() { return this.title || 'Counter' }
+    },
 };
 </script>
 
 <style>
 button {
-  background-color: #64B687;
-  border-radius: 5px;
-  border: 10px solid white;
-  color: white;
-  cursor: pointer;
-  margin: 0 15px;
-  padding: 5px 15px;
-  transition: 0, 3s ease-in-out;
+    background-color: #64B687;
+    border-radius: 5px;
+    border: 10px solid white;
+    color: white;
+    cursor: pointer;
+    margin: 0 15px;
+    padding: 5px 15px;
+    transition: 0, 3s ease-in-out;
 }
 
 button:hover {
-  background-color: #78c5e4;
-  border-radius: 7px;
-  transition: 0, 3s ease-in-out;
+    background-color: #78c5e4;
+    border-radius: 7px;
+    transition: 0, 3s ease-in-out;
 }
 </style>
