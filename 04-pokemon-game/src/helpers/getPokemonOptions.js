8edc1c32d5efbd4ddb933f1de62cf32fdef66d03
Juan Carlos Santamaría Villegas <jsantamv@gmail.com>
2023-01-25 16:46:09 -0600

    actualizacion

--- 04-pokemon-game/src/helpers/getPokemonOptions.js
+++ 04-pokemon-game/src/helpers/getPokemonOptions.js
@@ -6,15 +6,33 @@ const getPokemons = () => {
     return pokemonsArr.map((_, index) => index + 1)
 }
 
-const getPokemonsOptions = () => {
+const getPokemonsOptions = async () => {
     const mixedPokemons = getPokemons().sort(() => Math.random() - 0.5)
-    getPokemonsNames(mixedPokemons.splice(0, 4))
+    const pokemons = await getPokemonsNames(mixedPokemons.splice(0, 4))
+    return pokemons
 }
 
 const getPokemonsNames = async ([a, b, c, d] = []) => {
-    const result = await pokemonApi.get(`\1`)
-    console.log(result);
-    console.log(a, b, c, d)
+    // const result = await pokemonApi.get(`/1`)
+    // console.log(result);
+    // console.log(a, b, c, d)
+
+    const promiseArr = [
+        pokemonApi.get(`/${a}`),
+        pokemonApi.get(`/${b}`),
+        pokemonApi.get(`/${c}`),
+        pokemonApi.get(`/${d}`),
+    ]
+
+    const [p1, p2, p3, p4] = await Promise.all(promiseArr)
+
+    return [
+        { name: p1.data.name, id: p1.data.id },
+        { name: p2.data.name, id: p2.data.id },
+        { name: p3.data.name, id: p3.data.id },
+        { name: p4.data.name, id: p4.data.id }
+    ]
+    //    console.log(pok1, pok2, pok3, pok4)
 }
 
 export default getPokemonsOptions
--- 04-pokemon-game/src/pages/PokemonPage.vue
+++ 04-pokemon-game/src/pages/PokemonPage.vue
@@ -1,22 +1,63 @@
 <template>
-  <h1>Quien es ese Pokemon?</h1>
+    <h1 v-if="!pokemon">Espere por favor ...</h1>
+    <div v-else="pokemon">
+        <h1>Quien es ese Pokemon?</h1>
+        <PokemonPicture :pokemonId="pokemon.id" :showpokemon="showpokemon" />
+        <PokemonOption :pokemons="pokemonArr" @selection-pokemon="checkAnswer" />
+    </div>
 
-  <PokemonPicture :pokemonId="4" :showpokemon="true"/>
-  <PokemonOption/>
+    <div v-if="showAnswer" class="fade-in">
+        <h2 v>{{ message }}</h2>
+        <button @click="newGame">
+            Nuevo Juego
+        </button>
+    </div>
 
 </template>
 
 <script>
 import PokemonOption from '@/components/PokemonOption';
 import PokemonPicture from '@/components/PokemonPicture';
-import getPokemonOptions from '@/helpers/getPokemonOptions'
-
-
-console.log(getPokemonOptions());
-
+import getPokemonsOptions from '@/helpers/getPokemonOptions';
 
 export default {
-    components: { PokemonOption, PokemonPicture }
+    components: { PokemonOption, PokemonPicture },
+    data() {
+        return {
+            pokemonArr: [],
+            pokemon: null,
+            showpokemon: false,
+            showAnswer: false,
+            message: ''
+        }
+    },
+    methods: {
+        async mixPokemonArray() {
+            this.pokemonArr = await getPokemonsOptions()
+            const rndInt = Math.floor(Math.random() * 4)
+            this.pokemon = this.pokemonArr[rndInt]
+        },
+        checkAnswer(selectedId) {
+            this.showpokemon = true
+            this.showAnswer = true
+
+            if (selectedId === this.pokemon.id)
+                this.message = `Correcto, ${this.pokemon.name}`
+            else
+                this.message = `Opps, era ${this.pokemon.name}`
+        },
+        newGame() {
+            this.pokemon = null,
+            this.showAnswer = false
+            this.pokemonArr = []
+            this.pokemon = null
+            this.mixPokemonArray()
+        }
+    },
+    mounted() {
+        this.mixPokemonArray()
+    }
+
 }
 </script>
 
