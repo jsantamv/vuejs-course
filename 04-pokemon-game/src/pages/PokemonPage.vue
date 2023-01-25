c161259ca8f38de97353b4c5306c492c40a30357
Juan Carlos Santamaría Villegas <jsantamv@gmail.com>
2023-01-25 11:49:24 -0600

    update

---
+++ 04-pokemon-game/src/pages/PokemonPage.vue
@@ -0,0 +1,20 @@
+<template>
+  <h1>Quien es ese Pokemon?</h1>
+
+  <PokemonPicture/>
+  <PokemonOption/>
+
+</template>
+
+<script>
+import PokemonOption from '@/components/PokemonOption';
+import PokemonPicture from '@/components/PokemonPicture';
+
+export default {
+    components: { PokemonOption, PokemonPicture }
+}
+</script>
+
+<style>
+
+</style>
