4874a7e2e17a28b13b2ee605e89ed7b11ae8afdd
Juan Carlos Santamaría Villegas <jsantamv@gmail.com>
2023-01-26 15:44:30 -0600

    ACTUALIZACION

--- 04-pokemon-game/tests/unit/api/pokemonApi.spec.js
+++ 04-pokemon-game/tests/unit/api/pokemonApi.spec.js
@@ -2,6 +2,7 @@ import pokemonApi from "@/api/pokemonApi"
 
 describe('Pokemon API Test', () => {
     test('axios debe de estar configurado con el API de Pokemon', () => {
-        console.log(pokemonApi);
+        const baseUrl = 'https://pokeapi.co/api/v2/pokemon'
+        expect(pokemonApi.defaults.baseURL).toBe(baseUrl)
     })
 })
