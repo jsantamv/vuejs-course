73277c14317851a6bfc6bb890947e3eb50ea8dd0
Juan Carlos Santamaría Villegas <jsantamv@gmail.com>
2023-01-27 15:33:24 -0600

    update emit

--- 04-pokemon-game/tests/unit/components/PokemonOption.spec.js
+++ 04-pokemon-game/tests/unit/components/PokemonOption.spec.js
@@ -33,12 +33,23 @@ describe('PokemonOption', () => {
     })
 
     test('Debe de emite "selection-pokemon" con sus respectivos parametros al hacer click', () => {
-        const [li1,li2, li3, li4] = wrapper.findAll('li')
+        const [li1, li2, li3, li4] = wrapper.findAll('li')
 
-        li1.trigger
+        li1.trigger('click')
+        li2.trigger('click')
+        li3.trigger('click')
+        li4.trigger('click')
+
+        console.log(wrapper.emitted('selection-pokemon'));
+
+        expect(wrapper.emitted('selection-pokemon').length).toBe(4)
+        expect(wrapper.emitted('selection-pokemon')[0]).toEqual([1])
+        expect(wrapper.emitted('selection-pokemon')[1]).toEqual([2])
+        expect(wrapper.emitted('selection-pokemon')[2]).toEqual([3])
+        expect(wrapper.emitted('selection-pokemon')[3]).toEqual([4])
 
     })
 
-    
+
 
 })
