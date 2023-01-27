aba6876568078d50f7533807521c5ae57ed5a4e2
Juan Carlos Santamaría Villegas <jsantamv@gmail.com>
2023-01-27 17:35:44 -0600

    test

--- 04-pokemon-game/tests/unit/pages/PokemonPage.spec.js
+++ 04-pokemon-game/tests/unit/pages/PokemonPage.spec.js
@@ -1,22 +1,81 @@
 import { shallowMount, mount } from '@vue/test-utils'
 import PokemonPage from '@/pages/PokemonPage'
+import { pokemons } from '../mocks/pokemons.mock'
+import { shallowReactive } from 'vue'
 
 
 
-describe('PokemonPage', ()=> {
+describe('PokemonPage', () => {
     let wrapper
+    let mixPokemonArraySpy
 
-    beforeEach(()=>{
+    beforeEach(() => {
+        //creamos un spy, hay que llamarlo antes de que se monte
+        //cuando lo utilizamos aca debemos de limipar el spy
+        //mixPokemonArraySpy = jest.spyOn(PokemonPage.methods, 'mixPokemonArray')
+
         wrapper = shallowMount(PokemonPage)
     })
 
-    test('debe de hacer match con el snapshot', () => { 
+    test('debe de hacer match con el snapshot', () => {
         expect(wrapper.html()).toMatchSnapshot()
-     })
+    })
 
-     test('debe de llamar a mixPokemonArray al montar', () => { 
-            //creamos un spy
+    test('debe de llamar a mixPokemonArray al montar', () => {
+        const mixPokemonArraySpy = jest.spyOn(PokemonPage.methods, 'mixPokemonArray')
+        const wrapper = shallowMount(PokemonPage)
 
-            const mixPokemonArraySpy = jest.spyOn(PokemonPage.methods,'mixPokemonArray')
+        expect(mixPokemonArraySpy).toHaveBeenCalled()
+
+    })
+
+    test('debe de hacer match con el snapshot cuando carga los pokemon', () => {
+        //con esto monto el componente.
+        const wrapper = shallowMount(PokemonPage, {
+            //const wrapper = mount(PokemonPage, {
+            data() {
+                return {
+                    pokemonArr: pokemons,
+                    pokemon: pokemons[1],
+                    showpokemon: false,
+                    showAnswer: false,
+                    message: ''
+                }
+            }
+        })
+        expect(wrapper.html()).toMatchSnapshot()
+    })
+
+    test('debe de mostra los componentes de PokemonPincture y pokemon Options', () => {
+
+        const wrapper = shallowMount(PokemonPage, {
+            data() {
+                return {
+                    pokemonArr: pokemons,
+                    pokemon: pokemons[1],
+                    showpokemon: true,
+                    showAnswer: false,
+                    message: ''
+                }
+            }
+        })
+
+        //=========< pokemon picture >=========
+        //PokemonPicture debe de existir
+        const pokemonPicture = wrapper.find('pokemon-picture-stub')
+        expect(pokemonPicture.exists()).toBeTruthy()
+
+        //PokemonPicture attribute pokemonid === '5'        
+        expect(pokemonPicture.attributes('pokemonid')).toBe('2')
+
+        //=========< pokemon options >=========
+        //PokemonOptions debe de existir        
+        const pokemonOption = wrapper.find('pokemon-option-stub')
+        expect(pokemonOption.exists()).toBeTruthy()
+
+        //PokemonOptions attribute pokemons tobe true
+        expect(pokemonOption.attributes('pokemons')).toBeTruthy()
+
+
     })
 })
