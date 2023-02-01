1f855124b719ec0d6543ac00b31d64a4f107a1f7
Juan Carlos Santamaría Villegas <jsantamv@gmail.com>
2023-02-01 10:45:43 -0600

    update

--- 06-bases-vuex/src/components/FirstCounter.vue
+++ 06-bases-vuex/src/components/FirstCounter.vue
@@ -1,15 +1,32 @@
 <template>
     <h1>Counter - vuex</h1>
+
+    <h1>Formas de llamar el STATE</h1>
+
     <h2>Direct access: {{ $store.state.count }}</h2>
     <h2>Computed: {{ countComputed }}</h2>
+
+    <br>
+    <h1>mapState</h1>
+    <h2>mapState: {{ count }}</h2>
 </template>
 
 <script>
+
+//map state para mapear todo el state del store
+
+import { mapState } from 'vuex'
+
 export default {
     computed: {
         countComputed() {
             return this.$store.state.count
         }
-    }
+    },
+    ...mapState({
+        count: state => state.count,
+        // lastMutation: state => state.lastMutation
+        lastMutation: 'lastMutation'
+    })
 }
 </script>
