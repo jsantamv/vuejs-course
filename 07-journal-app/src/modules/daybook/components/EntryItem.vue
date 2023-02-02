1e85ae3c2c089f35c26e3a8f4a7a8e26e5d4c2aa
Juan Carlos Santamaría Villegas <jsantamv@gmail.com>
2023-02-02 17:08:21 -0600

    update data

--- 07-journal-app/src/modules/daybook/components/EntryItem.vue
+++ 07-journal-app/src/modules/daybook/components/EntryItem.vue
@@ -5,16 +5,35 @@
             <span class="mx-1 fs-5">Julio</span>
             <span class="mx-2 fw-light">2021, jueves</span>
         </div>
+
+        <div class="entry-description">
+            <p> Labore laboris mollit mollit nulla ullamco elit excepteur esse anim enim quis id est dolore.</p>
+            <br>
+            <p> Labore laboris mollit mollit nulla ullamco elit excepteur esse anim enim quis id est dolore.</p>
+        </div>
     </div>
 </template>
 
 <script>
-    export default {
+export default {
 
-    }
+}
 
 </script>
 
-<style>
+<style lang="scss" scoped>
+.entry-container {
+    border-bottom: 1px solid #3c3e50;
+    transition: 0.2s all ease-in;
 
+    &:hover {
+        background-color: lighten($color: gray, $amount: 45);
+        transition: 0.2s all ease-in;
+
+    }
+
+    .entry-description {
+        
+    }
+}
 </style>
