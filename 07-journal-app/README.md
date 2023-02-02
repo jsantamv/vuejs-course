05e8eb3a9fa889585f1d56fcdb1ca8ae2d548b4d
Juan Carlos Santamaría Villegas <jsantamv@gmail.com>
2023-02-02 12:48:41 -0600

    update project

--- 07-journal-app/README.md
+++ 07-journal-app/README.md
@@ -22,3 +22,13 @@ yarn lint
 
 ### Customize configuration
 See [Configuration Reference](https://cli.vuejs.org/config/).
+
+
+bootstrap@5.3.0-alpha1
+
+para trabajar con SASS en mi proyecto requiere realizar una instalacion de 
+loader SASS version 10 y sass, 
+
+```
+yarn add -D sass-loader@10 sass
+```
--- 07-journal-app/src/views/HomeView.vue
+++ 07-journal-app/src/views/HomeView.vue
@@ -1,5 +1,12 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
+
+    <div>
+      <h1>Hola Mundo</h1>
+      <button class="btn btn-primary m-2">Primary</button>
+      <button class="btn btn-secondary m-2">Secondary</button>
+      <button class="btn btn-success m-2">Success</button>
+    </div>
   </div>
 </template>
