e9d886246094493ff56a9e2f49338dc6d7ed14b3
Juan Carlos Santamaría Villegas <jsantamv@gmail.com>
2023-02-03 14:25:56 -0600

    update

--- 07-journal-app/src/modules/daybook/store/journal/getters.js
+++ 07-journal-app/src/modules/daybook/store/journal/getters.js
@@ -1,3 +1,11 @@
 // export const muGetters = (state) => {
     // return state.....
 // }
+
+export const getEntriesByTerm = (/*state*/) => {
+    
+}
+
+export const getEntryById = (/*state*/) => {
+    
+}
