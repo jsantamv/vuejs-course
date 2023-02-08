6f0828787b91e6d07f7bbeb622fe6aa734969390
Juan Carlos Santamaría Villegas <jsantamv@gmail.com>
2023-02-08 14:22:37 -0600

    udate

--- 07-journal-app/tests/unit/views/HomeView.spec.js
+++ 07-journal-app/tests/unit/views/HomeView.spec.js
@@ -3,13 +3,33 @@ import HomeView from '@/views/HomeView'
 
 describe('HomeView.vue', () => {
 
-    let wrapper
+    // let wrapper
 
-    beforeEach(() => {
-        wrapper = shallowMount(HomeView)
-    })
-    
+    // beforeEach(() => {
+    //     wrapper = shallowMount(HomeView)
+    // })
+
     test('debe de renderizar el componente correctamente', () => {
+        const wrapper = shallowMount(HomeView)
         expect(wrapper.html()).toMatchSnapshot()
     })
+
+    test('hacer click en un boton debe de redireccionar a no-entry', () => {
+        const mockRouter = {
+            push: jest.fn()
+        }
+
+        const wrapper = shallowMount(HomeView, {
+            global: {
+                $router: mockRouter
+            }
+        })
+
+        wrapper.find('button').trigger('click')
+        expect(mockRouter.push).toHaveBeenCalledTimes(0)
+        expect(mockRouter.push).toHaveBeenCalledWith({ name: 'no-entry' })
+
+    })
+
+
 })
