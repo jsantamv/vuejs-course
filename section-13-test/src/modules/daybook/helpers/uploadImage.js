aab46a1219eb7ac2887a98470c446483c3822907
Juan Carlos Santamaría Villegas <jsantamv@gmail.com>
2023-02-08 14:48:34 -0600

    update Journal with test

--- section-13-test/src/modules/daybook/helpers/uploadImage.js
+++ section-13-test/src/modules/daybook/helpers/uploadImage.js
@@ -1,19 +1,19 @@
 import axios from 'axios'
 
 
-const uploadImage = async ( file ) => {
+const uploadImage = async (file) => {
 
-    if ( !file ) return
+    if (!file) return
 
     try {
-        
+
         const formData = new FormData()
-        formData.append('upload_preset','curso-vue')
-        formData.append('file', file )
+        formData.append('upload_preset', 'curso-vue')
+        formData.append('file', file)
 
-        const url = 'https://api.cloudinary.com/v1_1/dx0pryfzn/image/upload'
+        const url = 'https://api.cloudinary.com/v1_1/dlkeaojyg/image/upload'
         const { data } = await axios.post(url, formData)
-
+        
         return data.secure_url
 
     } catch (error) {
