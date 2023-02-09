791641747a9ab9d0595c7d087bdcd7832a61a1cb
Juan Carlos Santamaría Villegas <jsantamv@gmail.com>
2023-02-09 15:33:59 -0600

    update

--- 08-compo-api/src/views/Counter.vue
+++ 08-compo-api/src/views/Counter.vue
@@ -1,1 +1,40 @@
-Vue.co
+<template lang="">
+    <div>
+        <h2>{{counter}}</h2>
+
+        <button @click="increase">+1</button>
+        <button @click="decrease">-1</button>
+    </div>
+</template>
+<script>
+import { ref } from 'vue';
+
+export default {
+    name: 'Counter',
+    props: {},
+    emits: [],
+    setup() {
+        const counter = ref(6)
+
+        const increase = () => {
+            counter.value++
+        }
+
+        const decrease = () => {
+            counter.value++
+        }
+
+
+        return {
+            //obj reactivos
+            counter,
+            //methods
+            increase,
+            decrease
+        }
+    }
+}
+</script>
+<style lang="">
+    
+</style>
