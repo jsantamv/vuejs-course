caa0673569b658a657e3ff35e05860affc475285
Juan Carlos Santamaría Villegas <jsantamv@gmail.com>
2023-02-09 16:42:29 -0600

    update date

--- 08-compo-api/src/views/AboutView.vue
+++ 08-compo-api/src/views/AboutView.vue
@@ -1,5 +1,76 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <h1>{{ title }}</h1>
+    <button @click="onChangeTitle">Cambiar</button>
   </div>
 </template>
+
+<script>
+import {
+  ref,
+  onActivated,
+  onBeforeMount,
+  onBeforeUnmount,
+  onBeforeUpdate,
+  onDeactivated,
+  onErrorCaptured,
+  onMounted,
+  onRenderTracked,
+  onRenderTriggered,
+  onUnmounted,
+  onUpdated
+} from 'vue'
+
+export default {
+  setup() {
+
+    let title = ref('about')
+
+    onBeforeMount(() => {
+      console.log('onBeforeMount')
+    })
+
+    onMounted(() => {
+      console.log('onMounted')
+    })
+    onBeforeUpdate(() => {
+      console.log('onBeforeUpdate')
+    })
+    onUpdated(() => {
+      console.log('onUpdated')
+    })
+    onBeforeUnmount(() => {
+      console.log('onBeforeUnmount')
+    })
+    onUnmounted(() => {
+      console.log('onUnmounted')
+    })
+    onErrorCaptured(() => {
+      console.log('onErrorCaptured')
+    })
+    onRenderTracked(() => {
+      console.log('onRenderTracked')
+    })
+    onRenderTriggered(() => {
+      console.log('onRenderTriggered')
+    })
+    onActivated(() => {
+      console.log('onActivated')
+    })
+    onDeactivated(() => {
+      console.log('onDeactivated')
+    })
+
+    return {
+      title,
+      onChangeTitle: () => title.value = 'New Titile'
+
+    }
+
+  }
+}
+</script>
+
+<style>
+
+</style>
