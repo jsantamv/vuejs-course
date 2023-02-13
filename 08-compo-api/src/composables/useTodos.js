bfbaf905b4bd2a3882c745a7dd7810e283b7075c
Juan Carlos Santamaría Villegas <jsantamv@gmail.com>
2023-02-13 11:09:18 -0600

    actualizacion

---
+++ 08-compo-api/src/composables/useTodos.js
@@ -0,0 +1,22 @@
+import { computed, ref } from 'vue'
+import { useStore } from 'vuex'
+
+const useTodos = () => {
+    //composables
+    const store = useStore()
+    const currentTab = ref('all')
+
+    return {
+        currentTab,
+
+        pending: computed(() => store.getters['pendingTodos']),
+        all: computed(() => store.getters['all']),
+        completed: computed(() => store.getters['completed']),
+        getTodosbyTab: computed(() => store.getters['getTodosbyTab'](currentTab.value)),
+
+        //methods
+        toggleTodo: (id) => store.commit('toggleTodo', id),
+    }
+}
+
+export default useTodos
