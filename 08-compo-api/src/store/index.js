83684eac83f56ef538e545ea1de3b7f90cad156e
Juan Carlos Santamaría Villegas <jsantamv@gmail.com>
2023-02-13 11:03:36 -0600

    update

--- 08-compo-api/src/store/index.js
+++ 08-compo-api/src/store/index.js
@@ -15,16 +15,33 @@ export default createStore({
       return state.todos.filter(todo => !todo.completed)
     },
 
-    all(state, getters, rootState){
+    all(state, getters, rootState) {
       return state.todos
     },
 
-    completed(state, getters, rootState){
+    completed(state, getters, rootState) {
       return state.todos.filter(todo => todo.completed)
+    },
+
+    /** puedo poner un guion bajo 
+     * si el parametro no lo necesito 
+     * en este caso no necesito el 'state'
+     * */
+    getTodosbyTab: (_, getters) => (tab) => {
+      switch (tab) {
+        case 'all': return getters.all
+        case 'pending': return getters.pendingTodos
+        case 'completed': return getters.completed
+      }
     }
 
   },
   mutations: {
+    toggleTodo(state, id) {
+      const todoIdx = state.todos.findIndex(todo => todo.id === id)
+
+      state.todos[todoIdx].completed = !state.todos[todoIdx].completed
+    }
   },
   actions: {
   },
