22388b51109a244b0949b1bb9f7f679c634b573d
Juan Carlos Santamaría Villegas <jsantamv@gmail.com>
2023-02-15 15:01:06 -0600

    Create my first module

---
+++ 15-auth-compo-api/src/modules/auth/store/state.js
@@ -0,0 +1,6 @@
+export default () => ({
+    status: 'authenticating', //'authenticated', 'not-authenticating','authenticating'
+    user: null,
+    idToken: null,
+    resfreshToken: null
+})
--- 15-auth-compo-api/src/store/index.js
+++ 15-auth-compo-api/src/store/index.js
@@ -1,11 +1,13 @@
 import { createStore } from 'vuex'
 
+import auth from '../modules/auth/store'
 import journal from '../modules/daybook/store/journal'
 
 
 const store = createStore({
     modules: {
-        journal
+        journal,
+        auth
     }
 })
 
