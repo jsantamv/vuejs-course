19c3ccf6896db4062d7bd73d470d89fd49380fa4
Juan Carlos Santamaría Villegas <jsantamv@gmail.com>
2023-02-15 14:43:53 -0600

    update

--- 15-auth-compo-api/src/router/index.js
+++ 15-auth-compo-api/src/router/index.js
@@ -1,6 +1,7 @@
 import { createRouter, createWebHashHistory } from 'vue-router'
 import Home from '../views/Home.vue'
 
+import authRouter from '../modules/auth/router'
 import daybookRouter from '../modules/daybook/router'
 
 const routes = [
@@ -16,6 +17,10 @@ const routes = [
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+  },
+  {
+    path: '/auth',
+    ...authRouter
   },
   {
     path: '/daybook',
