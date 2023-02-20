6bce5f149424e467563e6f1d678ad407f340c629
Juan Carlos Santamaría Villegas <jsantamv@gmail.com>
2023-02-20 13:22:01 -0600

    ejemplos de flex

--- quasar-project/src/pages/FlexPage.vue
+++ quasar-project/src/pages/FlexPage.vue
@@ -1,6 +1,99 @@
 <template>
-  <q-page class="flex">
-   <h1>Flex</h1>
+  <q-page>
+
+    <div class="row q-mb-sm">
+      <div class="col bg-primary"></div>
+      <div class="col bg-secondary"></div>
+      <div class="col bg-accent"></div>
+    </div>
+
+    <div class="row q-mb-md">
+      <div class="col bg-primary"></div>
+      <div class="col col-6 bg-secondary"></div>
+      <div class="col col-5 bg-accent"></div>
+    </div>
+
+    <div class="row q-mb-md justify-center">
+      <div class="col col-4 bg-primary"></div>
+      <div class="col col-4 bg-secondary"></div>
+    </div>
+
+    <div class="row q-mb-md justify-end">
+      <div class="col col-4 bg-primary"></div>
+      <div class="col col-4 bg-secondary"></div>
+    </div>
+
+    <div class="row q-mb-md justify-between">
+      <div class="col-12 col-md-6 col-sm-4 bg-primary"></div>
+      <div class="col-12 col-md-6 col-sm-4 bg-secondary"></div>
+    </div>
+
+    <q-separator spaced />
+
+    <div class="row q-mb-md justify-between">
+      <div class="col-12 col-sm-6 col-md-4 bg-primary"></div>
+      <div class="col-12 col-sm-6 col-md-4 bg-secondary"></div>
+    </div>
+
+    <q-separator spaced />
+
+    <div class="row q-mb-md">
+      <div class="square-box bg-primary"></div>
+      <div class="square-box bg-secondary"></div>
+      <div class="square-box bg-accent"></div>
+    </div>
+
+
+    <div class="row q-mb-md justify-between">
+      <div class="square-box bg-primary"></div>
+      <div class="square-box bg-secondary"></div>
+      <div class="square-box bg-accent"></div>
+    </div>
+
+
+    <div class="row q-mb-md justify-end">
+      <div class="square-box bg-primary"></div>
+      <div class="square-box bg-secondary"></div>
+      <div class="square-box bg-accent"></div>
+    </div>
+
+    <div class="row q-mb-md justify-around">
+      <div class="square-box bg-primary"></div>
+      <div class="square-box bg-secondary"></div>
+      <div class="square-box bg-accent"></div>
+    </div>
+
+    <div class="column q-mb-md items-center bg-dark">
+      <div class="square-box q-mb-md bg-primary"></div>
+      <div class="square-box q-mb-md bg-secondary"></div>
+      <div class="square-box q-mb-md bg-accent"></div>
+    </div>
+
+    <div class="column q-mb-md items-end bg-dark">
+      <div class="square-box q-mb-md bg-primary"></div>
+      <div class="square-box q-mb-md bg-secondary"></div>
+      <div class="square-box q-mb-md bg-accent"></div>
+    </div>
+
+    <div class="column custom-height q-mb-md items-center justify-center bg-dark">
+      <div class="square-box q-mb-md bg-primary"></div>
+      <div class="square-box q-mb-md bg-secondary"></div>
+      <div class="square-box q-mb-md bg-accent"></div>
+    </div>
+
+    <div class="column custom-height q-mb-md items-center justify-between bg-dark">
+      <div class="square-box q-mb-md bg-primary"></div>
+      <div class="square-box q-mb-md bg-secondary"></div>
+      <div class="square-box q-mb-md bg-accent"></div>
+    </div>
+
+    <div class="column custom-height q-mb-md items-center justify-between bg-dark">
+      <div class="square-box q-mb-md bg-primary"></div>
+      <div class="square-box q-mb-md bg-secondary self-end"></div>
+      <div class="square-box q-mb-md bg-accent"></div>
+    </div>
+
+
   </q-page>
 </template>
 
@@ -11,3 +104,22 @@ export default defineComponent({
   name: 'flex-page'
 })
 </script>
+
+<style scoped>
+.col,
+col-4,
+.col-6,
+.col-12 {
+  height: 50px;
+}
+
+.square-box {
+  width: 50px;
+  height: 50px;
+}
+
+.custom-height {
+  height: 300px;
+
+}
+</style>
