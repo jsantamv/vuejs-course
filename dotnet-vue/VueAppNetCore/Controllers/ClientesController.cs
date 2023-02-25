5b2550a6e3729a43b46e7de24a885a6071c900e5
Juan Carlos Santamaría Villegas <jsantamv@gmail.com>
2023-02-25 14:52:34 -0600

    actualizacion

---
+++ dotnet-vue/VueAppNetCore/Controllers/ClientesController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using VueAppNetCore.Models;
+
+namespace VueAppNetCore.Controllers
+{
+    public class ClientesController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public JsonResult GetClientes()
+        {
+            //List<Cliente> clientes = new Cliente().GetClientes();
+            //return Json(clientes);
+            return null;
+        }
+    }
+}
